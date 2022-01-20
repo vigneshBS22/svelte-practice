60253b23f78b9f55ac9bc1c54fda6929af0a910b
vigneshBS22 <vigneshb@geekyants.comm>
2022-01-20 17:57:06 +0530

    initial commit

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Card from './Card.svelte'
+    export let fb={};
+</script>
+
+<Card>
+    <div class="num-display">
+        {fb.rating}
+      </div>
+      <p class="text-display">
+        {fb.text}
+      </p>
+</Card>
+
+<style>
+    .num-display {
+      position: absolute;
+      top: -10px;
+      left: -10px;
+      width: 50px;
+      height: 50px;
+      background: #ff6a95;
+      color: #fff;
+      border: 1px #eee solid;
+      border-radius: 50%;
+      padding: 10px;
+      text-align: center;
+      font-size: 19px;
+    }
+  </style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+    import FeedbackItem from './FeedbackItem.svelte'
+    import {FeedbackStore} from  '../stores'
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <FeedbackItem {fb}/>
+{/each}
